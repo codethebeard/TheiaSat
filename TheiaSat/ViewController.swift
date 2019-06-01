db31eb943f6c4493fee808aa6df88fd4173408c1
Michael VanDyke <michaeljamesvandyke@gmail.com>
2019-06-01 17:56:35 -0400

    Initial Commit

---
+++ TheiaSat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheiaSat
+//
+//  Created by Michael VanDyke on 6/1/19.
+//  Copyright © 2019 Michael VanDyke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
